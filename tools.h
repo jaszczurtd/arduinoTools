f84445d8b6ab13c76f3391df744167b1117d0108
Marcin Kielesiński <marcin.kielesinski@b3group.pl>
2023-03-15 16:48:37 +0100

    arduino tools - initial commit

---
+++ tools.h
@@ -0,0 +1,39 @@
+#ifndef T_TOOOLS
+#define T_TOOOLS
+
+#include <Arduino.h>
+
+#ifndef ADC_BITS
+#define ADC_BITS 12
+#endif
+
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#ifndef NUMSAMPLES
+#define NUMSAMPLES 8
+#endif
+
+// The beta coefficient of the thermistor (usually 3000-4000)
+#ifndef BCOEFFICIENT
+#define BCOEFFICIENT 3600
+#endif
+
+// temp. for nominal resistance (almost always 25 C)
+#ifndef TEMPERATURENOMINAL
+#define TEMPERATURENOMINAL 21   
+#endif
+
+void deb(const char *format, ...);
+void floatToDec(float val, int *hi, int *lo);
+float adcToVolt(int adc, float r1, float r2);
+float ntcToTemp(int tpin, int thermistor, int r);
+int percentToGivenVal(float percent, int maxWidth);
+#ifdef I2C_SCANNER
+void i2cScanner(void);
+#endif
+float getAverageValueFrom(int tpin);
+unsigned long getSeconds(void);
+unsigned short byteArrayToWord(unsigned char* bytes);
+void wordToByteArray(unsigned short word, unsigned char* bytes);
+
+#endif
