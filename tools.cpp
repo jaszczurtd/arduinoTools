74691fdf02563da78c34f2130da8dc352e5c06fb
Marcin Kielesiński <marcin.kielesinski@b3group.pl>
2023-03-15 17:00:37 +0100

    small fixes for steinhart

--- tools.cpp
+++ tools.cpp
@@ -61,19 +61,18 @@ float getAverageValueFrom(int tpin) {
 
 float ntcToTemp(int tpin, int thermistor, int r) {
 
+    // Get the average value from the analog input
     float average = getAverageValueFrom(tpin);
 
-    // convert the value to resistance
-    average = 4095 / average - 1;
-    average = r / average;
+    // Convert the value to resistance
+    float resistance = (4095.0 / average - 1) * r;
 
-    float steinhart;
-    steinhart = average / thermistor;     // (R/Ro)
-    steinhart = log(steinhart);                  // ln(R/Ro)
-    steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+    // Calculate the temperature using the Steinhart-Hart equation
+    float steinhart = log(resistance / thermistor);  // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                      // 1/B * ln(R/Ro)
     steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-    steinhart = 1.0 / steinhart;                 // Invert
-    steinhart -= 273.15;                         // convert absolute temp to C    
+    steinhart = 1.0 / steinhart;                     // Invert
+    steinhart -= 273.15;                             // Convert absolute temp to C    
 
     return steinhart;
 }
